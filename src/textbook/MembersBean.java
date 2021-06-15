e18610a3beef51118bec961cce6eefaad7d105ff
RikiKotoku <85663088+RikiKotoku@users.noreply.github.com>
2021-06-15 11:13:20 +0900

    Add files via upload

---
+++ src/textbook/MembersBean.java
@@ -0,0 +1,106 @@
+package la.bean;
+
+import java.io.Serializable;
+
+public class MembersBean implements Serializable {
+	private int id;
+	private String family_name;
+	private String first_name;
+	private String postal;
+	private String address;
+	private String tel;
+	private String email;
+	private String birthday;
+	private String password;
+	private String year;
+	private String month;
+	private String day;
+
+
+
+
+	public String birthday(String year, String month, String day) {
+		this.year = year;
+		this.month = month;
+		this.day = day;
+		return year + month + day;
+	}
+
+	public MembersBean(int id, String family_name, String first_name, String postal, String address, String tel,
+			String email, String birthday, String password) {
+		super();
+		this.id = id;
+		this.family_name = family_name;
+		this.first_name = first_name;
+		this.postal = postal;
+		this.address = address;
+		this.tel = tel;
+		this.email = email;
+		this.birthday = birthday;
+		this.password = password;
+	}
+	public MembersBean(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	public MembersBean() {
+		super();
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFamily_name() {
+		return family_name;
+	}
+	public void setFamily_name(String family_name) {
+		this.family_name = family_name;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getPostal() {
+		return postal;
+	}
+	public void setPostal(String postal) {
+		this.postal = postal;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+
+}
---
+++ src/textbook/MembersDAO.java
@@ -0,0 +1,150 @@
+package la.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import la.bean.MembersBean;
+
+public class MembersDAO {
+	private Connection con;
+
+	public MembersDAO() throws DAOException {
+		getConnection();
+	}
+
+	public List<MembersBean> findAllMembers() throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+
+		try {
+			String sql = "SELECT * FROM members";
+			st = con.prepareStatement(sql);
+			rs = st.executeQuery();
+			List<MembersBean> list = new ArrayList<MembersBean>();
+			while (rs.next()) {
+				int id = rs.getInt("id");
+			    String family_name = rs.getString("family_name");
+			    String first_name = rs.getString("first_name");
+			    String postal = rs.getString("postal");
+			    String address = rs.getString("address");
+			    String tel = rs.getString("tel");
+			    String email = rs.getString("email");
+			    String birthday = rs.getString("birthday");
+			    String password = rs.getString("password");
+
+			    MembersBean bean = new MembersBean(id, family_name, first_name, postal, address, tel, email, birthday, password);
+			    list.add(bean);
+			}
+			return list;
+		} catch (SQLException e){
+			throw new DAOException("レコードの取得に失敗しました");
+		} finally {
+			try {
+				if (st != null) {
+					st.close();
+				}
+				if (rs != null) {
+					rs.close();
+				}
+				close();
+			} catch (SQLException e) {
+				throw new DAOException("リソースの開放に失敗しました");
+			}
+		  }
+	}
+
+	public int newMembers(MembersBean members) throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+
+		try {
+			String sql = "INSERT INTO members VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+			st = con.prepareStatement(sql);
+			st.setString(1, members.getFamily_name());
+			st.setString(2, members.getFirst_name());
+			st.setString(3, members.getPostal());
+			st.setString(4, members.getAddress());
+			st.setString(5, members.getTel());
+			st.setString(6, members.getEmail());
+			st.setString(7, members.getBirthday());
+			st.setString(8, members.getPassword());
+			int rows = st.executeUpdate();
+			return rows;
+		} catch (SQLException e){
+			throw new DAOException("レコードの取得に失敗しました");
+		} finally {
+			try {
+				if (st != null) {
+					st.close();
+				}
+				close();
+			} catch (SQLException e) {
+				throw new DAOException("リソースの開放に失敗しました");
+			}
+		  }
+	}
+
+	public int deleteMembers(MembersBean members) throws DAOException {
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+
+		try {
+			String sql = "DELETE FROM members Where id = ?";
+			st = con.prepareStatement(sql);
+			st.setInt(1, members.getId());
+			int rows = st.executeUpdate();
+			return rows;
+		} catch (SQLException e){
+			throw new DAOException("レコードの取得に失敗しました");
+		} finally {
+			try {
+				if (st != null) {
+					st.close();
+				}
+				close();
+			} catch (SQLException e) {
+				throw new DAOException("リソースの開放に失敗しました");
+			}
+		  }
+	}
+
+
+
+		private void getConnection() throws DAOException {
+			try {
+				Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException e) {
+				throw new DAOException("接続に失敗しました。");
+			}
+
+			String url = "jdbc:postgresql:sample";
+			String user = "student";
+			String pass = "himitu";
+
+			try {
+				con = DriverManager.getConnection(url, user, pass);
+			} catch (SQLException e) {
+				throw new DAOException("接続に失敗しました。");
+			}
+		}
+
+		private void close() throws SQLException {
+			if (con != null) {
+				con.close();
+				con = null;
+			}
+		}
+
+}
