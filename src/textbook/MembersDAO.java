8c5a1344c94a7f3114a35d9fa9c11372fb48c83d
Riki Kotoku <2021java52-student001@la-study.com>
2021-06-15 11:38:05 +0900

    0615/11:38

--- src/textbook/MembersDAO.java
+++ src/textbook/MembersDAO.java
@@ -1,4 +1,4 @@
-package la.dao;
+package textbook;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -7,8 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import la.bean.MembersBean;
 
 public class MembersDAO {
 	private Connection con;
