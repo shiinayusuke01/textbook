64da87e5f70c8133f9d3f505c03ad049d4d63be3
Riki Kotoku <2021java52-student001@la-study.com>
2021-06-15 12:04:26 +0900

    ore

--- src/textbook/MainPageServlet.java
+++ src/textbook/MainPageServlet.java
@@ -31,10 +31,10 @@ public class MainPageServlet extends HttpServlet {
 
 		try {
 			TextBookDAO dao = new TextBookDAO();
-			List<TextBookBean> list = dao.findAll();
+			List<TextbookBean> list = dao.findAll();
 			//リストをJSPに送る
 			request.setAttribute("TextBook", TextBook);
-			RequestDispatcher rd = request.getRequestDispatcher("/main-input.jsp);"
+			RequestDispatcher rd = request.getRequestDispatcher("/main_input.jsp);"
 			rd.forward(request,  response);
 		}catch(DAOException e) {
 			e.printStackTrace();
