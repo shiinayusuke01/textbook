48a4382a374f71b47ba6b1ac12d77150997cbf04
shuyayamada1 <85668748+shuyayamada1@users.noreply.github.com>
2021-06-16 14:40:10 +0900

    Add files via upload

---
+++ WebContent/MainPageServlet.java
@@ -0,0 +1,46 @@
+package textbook;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainPageServlet
+ */
+@WebServlet("/MainPageServlet")
+public class MainPageServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Object textbook;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		try {
+			textDAO dao = new textDAO();
+			List<TextbookBean> list = dao.findAll();
+			//リストをJSPに送る
+			request.setAttribute("textbook", list);
+			RequestDispatcher rd = request.getRequestDispatcher("/main-input.jsp");
+			rd.forward(request,  response);
+		}catch(DAOException e) {
+			e.printStackTrace();
+			request.setAttribute(" message", " 正しいタイトルを入力してください");
+			RequestDispatcher rd = request.getRequestDispatcher("/errInput.jsp");
+			rd.forward(request,  response);
+		}
+	}
+
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
+
