0600c5ba6acb2d3f7118e5548241654b0c4f391f
MiyuKuboki <85663101+MiyuKuboki@users.noreply.github.com>
2021-06-17 16:27:40 +0900

    TextBookDAOの　deletetextbookメソッドを追加

--- src/textbook/TextBookDAO.java
+++ src/textbook/TextBookDAO.java
@@ -251,7 +251,47 @@ public class TextBookDAO {
      		}
      	}
 	}
+	public int deletetextbook(String title, String author, int category, String status, int price, String info, int userId ) throws DAOException{
 
+		System.out.println(status);
+
+		if (con == null)
+			getConnection();
+
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+
+			// SQL文の作成
+			String sql = "DELETE FROM textbooks WHERE(?, ?, ?, ?, ?, ?, ?)";
+			st = con.prepareStatement(sql);
+			st.setString(1, title);
+			st.setString(2, author);
+			st.setInt(3, category);
+			st.setString(4, status);
+			st.setInt(5, price);
+			st.setString(6, info);
+			st.setInt(7, userId);
+
+			// SQLの実行
+			int rows = st.executeUpdate();
+
+			// カテゴリ一覧をListとして返す
+			return rows;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new DAOException("レコードの取得に失敗しました。");
+		} finally {
+			try {
+				// リソースの開放
+				if(rs != null) rs.close();
+				if(st != null) st.close();
+				close();
+			} catch (Exception e) {
+				throw new DAOException("リソースの開放に失敗しました。");
+			}
+		}
+	}
 	private void getConnection() throws DAOException {
 		try {
 			Class.forName("org.postgresql.Driver");
