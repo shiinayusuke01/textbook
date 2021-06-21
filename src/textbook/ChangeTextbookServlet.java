6cc2274968690b25dfcf5ee51231896086279c7d
Yusuke Shiina <2021java52-student013@la-study.com>
2021-06-21 11:04:45 +0900

    教科書の変更の箇所の実装

--- src/textbook/ChangeTextbookServlet.java
+++ src/textbook/ChangeTextbookServlet.java
@@ -1,7 +1,6 @@
 package textbook;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,16 +10,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class RegistTextbook
+ * Servlet implementation class ChangeTextbookServlet
  */
-@WebServlet("/RegistTextbook")
-public class RegistTextbook extends HttpServlet {
+@WebServlet("/ChangeTextbookServlet")
+public class ChangeTextbookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public RegistTextbook() {
+    public ChangeTextbookServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,12 +28,20 @@ public class RegistTextbook extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String action = request.getParameter("action");
-		System.out.println(action);
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
-		int userId = 1;
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html; charset=UTF-8");
+		int userid = 1;
+
+		int id = Integer.parseInt(request.getParameter("id"));
 		String title = request.getParameter("title");
 		String author = request.getParameter("author");
 		int category = Integer.parseInt(request.getParameter("category"));
@@ -42,25 +49,16 @@ public class RegistTextbook extends HttpServlet {
 		int price = Integer.parseInt(request.getParameter("price"));
 		String info = request.getParameter("info");
 
+		System.out.println(info);
 
-		PrintWriter out = response.getWriter();
-		out.print(title + category);
-
+		TextbookBean bean = new TextbookBean(id, title, author, category, status, price, info, userid);
 		try {
 			TextBookDAO dao = new TextBookDAO();
-			dao.newTextbook(title, author, category, status, price, info, userId );
+			dao.changeTextbook(bean);
 		}catch (DAOException e) {
 			e.printStackTrace();
 		}
 		gotoPage(request, response, "form-textbook.jsp");
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 	private void gotoPage(HttpServletRequest request, HttpServletResponse response, String page)throws ServletException, IOException{
 		RequestDispatcher rd = request.getRequestDispatcher(page);
